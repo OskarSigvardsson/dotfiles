c7d7a132c8f9a33575e877ffc7e2f801974b6d27
Oskar Sigvardsson <oskarsigvardsson@gmail.com>
2016-03-30 18:00:06 +0200

    Added subfolder install scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+# Link dotfiles
 for FILE in $( find * | grep "\.home$" )
 do
     DOTFILE=$( basename $FILE | sed "s/\(.*\)\.home$/.\1/" )
 
-    ln -s -i "`pwd`/$FILE" "$HOME/$DOTFILE"
     if [ ! -f "$HOME/$DOTFILE" ]
     then
         echo "$HOME/$DOTFILE replaced"
     fi
+    ln -s -i "`pwd`/$FILE" "$HOME/$DOTFILE"
+done
+
+# Run installer scripts
+for FILE in $( ls -d */ | sed 's|/$||' | xargs find | grep "install\.sh$" )
+do
+    sh $FILE
 done
