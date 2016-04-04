c18996c4a2798ca77dcb89e51834a6cb82691605
Oskar Sigvardsson <oskarsigvardsson@gmail.com>
2016-04-04 15:26:15 +0000

    Set right directory for bashrc script

--- bashrc.sh
+++ bashrc.sh
@@ -1,5 +1,7 @@
 # Run bashrc files from subdirectories. 
+cd `dirname ${BASH_SOURCE[0]}`
 for FILE in $( ls -d */ | sed 's|/$||' | xargs find | grep "bashrc\.sh$" )
 do
+    echo $FILE
     source $FILE
 done
