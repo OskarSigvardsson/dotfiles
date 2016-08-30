8f6df0a447b55f72ae3c78cef0039b356e97fec2
Oskar Sigvardsson <oskarsigvardsson@gmail.com>
2016-08-30 09:37:50 +0200

    Added a few aliases, configued vim for js

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -5,3 +5,7 @@ alias sudo='sudo '
 alias map="xargs -n1"
 
 alias timer='echo "Timer started. Stop with Ctrl-D." && date && time cat && date'
+
+# git aliases
+alias gs="git status"
+alias gc="git commit"
