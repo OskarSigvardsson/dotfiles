69a50a859ca62acd9db80f72aa6e6f8d0ee095e4
Oskar Sigvardsson <oskarsigvardsson@gmail.com>
2017-03-16 10:01:35 +0100

    Moved bash-prompt to this repo

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -9,6 +9,14 @@ then
 fi
 
 source bash/aliases.sh
-source bash/bash-powerline/bash-powerline.sh
+#source bash/bash-powerline/bash-powerline.sh
+
+function _update_ps1() {
+    PS1="$(~/.bash_prompt.py $?)"
+}
+
+if [ "$TERM" != "linux" ]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
 
 set PATH=$PATH:$HOME/.cargo/bin
