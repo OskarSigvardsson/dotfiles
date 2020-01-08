badf1e323b6ca36c7a6507e12409e3164a61074d
Oskar Sigvardsson <oskarsigvardsson@gmail.com>
2020-01-08 16:16:05 +0100

    Removed old custom stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,17 +18,3 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(package-selected-packages
-;;    (quote
-;;     (col-highlight crosshairs yasnippet-snippets which-key use-package shader-mode rmsbolt projectile org-bullets omnisharp nlinum neotree ledger-mode helm-ls-git hacker-typer general flycheck-ledger expand-region evil-org evil-magit evil-lion cquery company-tabnine company-lsp base16-theme))))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
